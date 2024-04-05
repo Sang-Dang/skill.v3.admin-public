e2c97f36ad6c8da11251224f5c8d7f80e1e9cfd6
Sang Dang <sangdase170287@fpt.edu.vn>
2024-04-05 23:32:03 +0700

    test123

--- src/routes/test/index.tsx
+++ src/routes/test/index.tsx
@@ -1,19 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Scanner } from '@yudiel/react-qr-scanner'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/test/')({
     component: Test,
 })
 
 function Test() {
+    const [data, setData] = useState<string | undefined>()
+
     return (
-        <div className='w-96'>
-            <Scanner
-                onResult={(text, result) => console.log(text, result)}
-                onError={(error) => console.log(error?.message)}
-                enabled
-                options={{}}
-            />
+        <div>
+            <div className='w-96'>
+                <Scanner
+                    onResult={(text) => {
+                        setData(text)
+                    }}
+                    onError={(error) => console.log(error?.message)}
+                    enabled
+                    options={{}}
+                />
+            </div>
+            <div className='mt-4'>{data}</div>
         </div>
     )
 }
