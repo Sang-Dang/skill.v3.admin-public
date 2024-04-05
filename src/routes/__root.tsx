cd32f0ecbdbf751c39ef04647b77011a35b5a953
Sang Dang <sangdase170287@fpt.edu.vn>
2024-04-05 15:04:29 +0700

    ...

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,12 +2,24 @@ import { AuthHandler } from '@/lib/AuthHandler'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { App, ConfigProvider } from 'antd'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { QueryClient } from '@tanstack/react-query'
 import { AuthContextProvider } from '@/common/context/AuthContext'
 import { authHandler } from '@/router'
 import { Suspense } from 'react'
 import { enUSIntl } from '@ant-design/pro-components'
+import React from 'react'
+
+const TanStackRouterDevtools =
+    process.env.NODE_ENV === 'production'
+        ? () => null // Render nothing in production
+        : React.lazy(() =>
+              // Lazy load in development
+              import('@tanstack/router-devtools').then((res) => ({
+                  default: res.TanStackRouterDevtools,
+                  // For Embedded Mode
+                  // default: res.TanStackRouterDevtoolsPanel
+              })),
+          )
 
 type RootContext = {
     authHandler: AuthHandler
